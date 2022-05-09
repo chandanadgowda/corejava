a7bed4cf05741c730b9c6816efb03a4805942272
chandanadgowda <chandanad7digital@gmail.com>
2022-05-09 10:02:28 +0530

    FIRSTcommit

---
+++ Training1/src/Sample/Sum.java
@@ -0,0 +1,13 @@
+package Sample;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		 int a=2, b=3,sum;
+		  sum=a+b;
+		  System.out.println("the sum is:"+sum);
+		    
+
+	}
+
+}
