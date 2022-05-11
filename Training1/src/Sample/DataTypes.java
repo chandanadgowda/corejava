b8495e3566e1883f0d54895dbfd53b008629f1e2
chandanadgowda <chandanad7digital@gmail.com>
2022-05-11 20:37:42 +0530

    first commit

---
+++ Training1/src/Sample/DataTypes.java
@@ -0,0 +1,11 @@
+package Sample;
+
+public class DataTypes {
+	public static void main(String args[]) {
+		int x=10,y=20;
+		System.out.println(x+y);
+		System.out.println(--x);
+		System.out.println(++y);
+	}
+
+}
