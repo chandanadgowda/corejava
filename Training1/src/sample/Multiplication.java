47809274f78fc6fdfd74419545ec89672e7f8fca
chandanadgowda <chandanad7digital@gmail.com>
2022-05-17 16:39:00 +0530

    homeworkof16

---
+++ Training1/src/sample/Multiplication.java
@@ -0,0 +1,19 @@
+package sample;
+
+import java.util.Scanner;
+
+public class Multiplication {
+	public static void main(String args[]){
+		int i;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number");
+		int n=sc.nextInt();
+		for(i=10;i>=1;i--) {
+			
+		
+		
+		System.out.println(+n*i);
+	}
+	
+	}
+}
---
+++ Training1/src/sample/RangeMultiplication.java
@@ -0,0 +1,21 @@
+package sample;
+
+import java.util.Scanner;
+
+public class RangeMultiplication {
+	public static void main(String args[]){
+		int i;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number");
+		int n=sc.nextInt();
+		System.out.println("enter the range");
+		int range=sc.nextInt();
+		for(i=1;i<=range;i++) {
+			
+		
+		
+		System.out.println(+n*i);
+	}
+	
+	}
+}
