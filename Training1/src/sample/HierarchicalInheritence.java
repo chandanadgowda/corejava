fa7964812c9a8849e1ab2746915b56d856a3f3e0
chandanadgowda <chandanad7digital@gmail.com>
2022-05-22 13:30:40 +0530

    HierarchicalInheritence

---
+++ Training1/src/sample/HierarchicalInheritence.java
@@ -0,0 +1,33 @@
+package sample;
+
+
+class Father {
+	  String familyName;
+	  String houseaddress;
+	  Father() {
+	    familyName = "Programmer";
+	    houseaddress = "Delhi";
+	  }
+	}
+	class Son extends Father {
+	  Son() {
+	    System.out.println("I am the Son");
+	    System.out.println("My family name is " + this.familyName + " and I am from " + this.houseaddress);
+
+	  }
+
+	}
+	class Daughter extends Father {
+	  Daughter() {
+	    System.out.println("I am the Daughter");
+	    System.out.println("My family name is " + this.familyName + " and I am from " + this.houseaddress);
+	  }
+	}
+	public class  HierarchicalInheritence {
+
+	
+	  public static void main(String[] args) {
+	    Son s = new Son();
+	    Daughter d = new Daughter();
+	  }
+	}
