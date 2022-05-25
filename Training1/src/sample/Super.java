bc0d6cd2e3480bf1529d6692d646c98f36d0cec8
chandanadgowda <chandanad7digital@gmail.com>
2022-05-25 11:34:14 +0530

    Super

---
+++ Training1/src/sample/Super.java
@@ -0,0 +1,49 @@
+package sample;
+
+
+	class A
+	{
+		int a;
+		int b;
+		public A()
+		{
+			System.out.println("In A class defailt constructor");	
+		}
+		public A(int a, int b)
+		{
+			System.out.println("In A class parameterized constructor");
+			this.a = a;
+			this.b = b;
+		}
+		
+	}
+	class B extends A
+	{
+		int c;
+
+		public B() 
+		{
+			super();
+			super.a=10;
+			super.b=20;
+			System.out.println("In B class default constructor");
+		}
+
+		public B(int c) 
+		{
+			super();
+			this.c = c;
+			System.out.println("In B class parameterized constructor");
+		}
+	}
+	public class Super 
+	{
+		public static void main(String[] args) 
+		{
+			B obj=new B();
+	        System.out.println(obj.a);
+	        System.out.println(obj.b);
+		}
+
+	}
+
