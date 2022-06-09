24271db456f0b7357234d5cc41eb0f383ec3768d
chandanadgowda <chandanad7digital@gmail.com>
2022-06-09 09:47:47 +0530

    TryCatch

---
+++ Training1/src/sample/Testtrycatch1.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class Testtrycatch1 {
+	public static void main(String args[])
+	{
+	int data=50/0; //may throw exception
+	System.out.println("rest of the code...");
+	}
+	}
